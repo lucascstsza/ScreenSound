997e6cf079adc11acc745987392eb9edfdbbcf57
Lucas Costa Souza <lucas_costasouza@yahoo.com.br>
2024-09-06 01:04:19 -0300

    Aula 04.06 - Excluindo uma música

--- ScreenSound.Web/Services/MusicasAPI.cs
+++ ScreenSound.Web/Services/MusicasAPI.cs
@@ -13,13 +13,24 @@ public class MusicasAPI
         _httpClient = factory.CreateClient("API");
     }
 
+    public async Task<MusicaResponse?> GetMusicaPorNomeAsync(string nome)
+    {
+        return await _httpClient.GetFromJsonAsync<MusicaResponse>($"musicas/{nome}");
+    }
     public async Task<ICollection<MusicaResponse>?> GetMusicasAsync()
     {
         return await _httpClient.GetFromJsonAsync<ICollection<MusicaResponse>>("musicas");
     }
-
     public async Task AddMusicaAsync(MusicaRequest musica)
     {
         await _httpClient.PostAsJsonAsync("musicas", musica);
+    }
+    public async Task DeleteMusicaAsync(int id)
+    {
+        await _httpClient.DeleteAsync($"musicas/{id}");
+    }
+    public async Task UpdateMusicaAsync(MusicaRequestEdit artista)
+    {
+        await _httpClient.PutAsJsonAsync($"musicas", artista);
     }
 }
