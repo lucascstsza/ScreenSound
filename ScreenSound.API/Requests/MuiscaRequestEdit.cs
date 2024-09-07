12331f98ac78fe45f18aad1dc1cf567deb43f0c9
Lucas Costa Souza <lucas_costasouza@yahoo.com.br>
2024-09-07 00:53:10 -0300

    Fix - Renomeando MusicaRequestEdit

--- ScreenSound.API/Requests/MuiscaRequestEdit.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace ScreenSound.API.Requests;
-
-public record MusicaRequestEdit(int Id, string Nome, int ArtistaId,int AnoLancamento) : MusicaRequest(Nome,ArtistaId,AnoLancamento);
---
+++ ScreenSound.API/Requests/MusicaRequestEdit.cs
@@ -0,0 +1,3 @@
+﻿namespace ScreenSound.API.Requests;
+
+public record MusicaRequestEdit(int Id, string Nome, int ArtistaId, int AnoLancamento) : MusicaRequest(Nome, ArtistaId, AnoLancamento);
