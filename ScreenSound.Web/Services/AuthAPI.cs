c2c9e073f1d1502851faab2408cd060eac7075cd
Lucas Costa Souza <lucas_costasouza@yahoo.com.br>
2024-09-09 01:14:08 -0300

    Aula 02.05 - Guardando o cookie

---
+++ ScreenSound.Web/Services/AuthAPI.cs
@@ -0,0 +1,25 @@
+﻿using ScreenSound.Web.Response;
+using System.Net.Http.Json;
+
+namespace ScreenSound.Web.Services;
+
+public class AuthAPI(IHttpClientFactory factory)
+{
+    private readonly HttpClient _httpclient = factory.CreateClient("API");
+
+    public async Task<AuthResponse> LoginAsync(string email, string senha)
+    {
+        var response = await _httpclient.PostAsJsonAsync("auth/login?useCookies=true", new
+        {
+            email,
+            password = senha
+        });
+
+        if (response.IsSuccessStatusCode)
+        {
+            return new AuthResponse { Sucesso = true };
+        }
+
+        return new AuthResponse { Sucesso = false, Erros = ["Login ou senha inválidos"] };
+    }
+}
